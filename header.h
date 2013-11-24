81e04210e1f5ba79845ea90413c5c49addbb9e96
gavin <861260447@qq.com>
2013-11-24 19:33:59 +0800

    new

--- header.h
+++ header.h
@@ -9,6 +9,7 @@ private:
         char sex;
         int service_years;
 public:
+        CStaff();
         int salary;
        int setNo();
        int setName();  //如何传字符串？
@@ -21,35 +22,37 @@ public:
        int getServiceYears();
        int getSalary();
 };
+CStaff::CStaff(){
+    this->salary=0;
+}
 int CStaff::setNo(){
     int id;
-    cout << "请输入员工的ID：" << endl;
+    cout << "Please input the ID：" << endl;
     cin >> id;
     this->staff_id=id;
     return 1;
 }
 int CStaff::setName(){
     string name;
-    cout << "请输入员工姓名：" << endl;
+    cout << "Name：" << endl;
     getline(cin,name);
     this->name=name;
     return 1;
 }
 int CStaff::setSex(){
     char sex;
-    cout << "员工性别：" << endl;
+    cout << "Sex：" << endl;
     cin >> sex;
     this->sex=sex;
     return 1;
 }
 int CStaff::setServiceYears(){
     int service_years;
-    cout << "员工工龄：" << endl;
+    cout << "Service Years：" << endl;
     cin >> service_years;
     this->service_years=service_years;
     return 1;
 }
-
 int CStaff::getNo(){
     return staff_id;
 }
@@ -83,7 +86,7 @@ private:
         int post_salary;
 public:
         CAdministrator();
-        int setPostSalary(int post_salary);
+        CAdministrator(int post);
         int getSalary();
 };
 CAdministrator::CAdministrator(){
@@ -92,19 +95,72 @@ CAdministrator::CAdministrator(){
     this->post_salary=0;
     this->salary=0;
 }
-int CAdministrator::setPostSalary(int post_salary){
-    this->post_salary=post_salary;
-    return 1;
+CAdministrator::CAdministrator(int post){
+    this->base_salary=1500;
+    this->base_service_salary=200;
+    this->post_salary=post;
+    this->salary=0;
 }
 int CAdministrator::getSalary(){
     int years=this->getServiceYears();
-    return this->base_salary+this->base_service_salary*years+this->post_salary;
+    return base_salary+base_service_salary*years+post_salary;
 }
+
 
 //教师(CStaff)
 class CTeacherL:public CStaff{
-
+private:
+        int base_salary;
+        int base_service_salary;
+        int class_salary;
+        int class_num;
+        int msr_salary;
+        int dor_salary;
+public:
+        CTeacherL();
+        CTeacherL(int msr);
+        CTeacherL(int msr,int dor);
+        int setClassNum();
+        int getSalary();
 };
+CTeacherL::CTeacherL(){
+    this->base_salary=2000;
+    this->base_service_salary=200;
+    this->class_salary=12;
+    this->class_num=0;
+    this->msr_salary=0;
+    this->dor_salary=0;
+    this->salary=0;
+}
+CTeacherL::CTeacherL(int msr){
+    this->base_salary=2000;
+    this->base_service_salary=200;
+    this->class_salary=15;
+    this->class_num=0;
+    this->msr_salary=msr;
+    this->dor_salary=0;
+    this->salary=0;
+}
+CTeacherL::CTeacherL(int msr,int dor){
+    this->base_salary=2000;
+    this->base_service_salary=200;
+    this->class_salary=20;
+    this->class_num=0;
+    this->msr_salary=msr;
+    this->dor_salary=dor;
+    this->salary=0;
+}
+int CTeacherL::setClassNum(){
+    int num;
+    cout << "Please input how many classes you teach :" << endl;
+    cin >> num;
+    this->class_num=num;
+    return 1;
+}
+int CTeacherL::getSalary(){
+    int years=this->getServiceYears();
+    return base_salary+base_service_salary*years+class_salary*class_num+msr_salary+dor_salary;
+}
 
 
 
